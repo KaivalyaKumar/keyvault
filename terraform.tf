090453e649c994b47ab07c851f4b915b276f754d
Kaivalya Kumar <81738764+KaivalyaKumar@users.noreply.github.com>
2026-02-17 17:26:34 +0530

    Add azurerm module dependency to terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -8,6 +8,10 @@ terraform {
       source = "localterraform.com/SSC/azapi"
       version = ">= 2.4.0"
     }
+    azurerm = {
+      source = "localterraform.com/SSC/azurerm"
+      version = ">= 2.4.0"
+    }
   }
   required_version = ">= 1.8.0"
 }
